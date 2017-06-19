d29b34b49333e0be65cb3c501c8579a744e1b6f2
Gerome Marc Braddock <BraddockG@users.noreply.github.com>
2017-06-19 15:29:38 +0100

    Changed some main classes

--- src/MainModel.java
+++ src/MainModel.java
@@ -1,4 +1,13 @@
+import checkout.CheckoutController;
 
 public class MainModel {
 
+	public void launchCheckout(String accommodation) {
+		new CheckoutController(accommodation);
+	}
+	
+	public void launchStaffArea() {
+		// TO DO
+	}
+	
 }
