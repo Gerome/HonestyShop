1b0d32eded6093f59fcd06cf366bf31a2920e88b
Gerome Marc Braddock <BraddockG@users.noreply.github.com>
2017-06-19 12:45:31 +0100

    Created product class
    Added unfinished constructor along with it

---
+++ src/product/Product.java
@@ -0,0 +1,20 @@
+package product;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+	private String productID, productName;
+	private BigDecimal buyPrice, sellPrice;
+	private int stockLevel, normalLevel;
+	
+	public Product(String productID, String productName,
+			BigDecimal buyPrice, BigDecimal sellPrice,
+			int stockLevel, int normalLevel) {
+		
+		
+	}
+	
+	
+	
+}
