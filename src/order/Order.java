0b3a5238d9838a202556562302be498c87bd57b7
Gerome Marc Braddock <BraddockG@users.noreply.github.com>
2017-06-19 17:26:25 +0100

    Created the order item

--- src/order/Order.java
+++ src/order/Order.java
@@ -1,5 +1,31 @@
 package order;
 
-public class Order {
+import java.util.Date;
 
+public class Order {
+	
+	private String orderID, accommodation, name;
+	private Date datetime;
+	private Double total;
+
+	public Order(String orderID, String accommodation, 
+			Date datetime, Double total, String name) {
+		this.orderID = orderID;
+		this.accommodation = accommodation;
+		this.datetime = datetime;
+		this.setTotal(total);
+		this.name = name;
+	}
+
+	Double getTotal() {
+		return total;
+	}
+
+	void setTotal(Double total) {
+		this.total = total;
+	}
+	
+	
+	
+	
 }
