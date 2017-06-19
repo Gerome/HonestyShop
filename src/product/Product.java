03c78b5f6ce355fe49f94dc0def1d1344418bd43
Gerome Marc Braddock <BraddockG@users.noreply.github.com>
2017-06-19 15:26:37 +0100

    Created getters and setters for products
    Temporarily changed the prices to doubles for testing purposes

--- src/product/Product.java
+++ src/product/Product.java
@@ -5,14 +5,56 @@ import java.math.BigDecimal;
 public class Product {
 
 	private String productID, productName;
-	private BigDecimal buyPrice, sellPrice;
+	private double buyPrice, sellPrice;
 	private int stockLevel, normalLevel;
 	
-	public Product(String productID, String productName,
-			BigDecimal buyPrice, BigDecimal sellPrice,
-			int stockLevel, int normalLevel) {
+	public Product(String productID, 
+			String productName,
+			double buyPrice, 
+			double sellPrice,
+			int stockLevel, 
+			int normalLevel) {
+		
+		this.productID = productID;
+		this.setProductName(productName);
+		this.buyPrice = buyPrice;
+		this.setSellPrice(sellPrice);
+		this.setStockLevel(stockLevel);
+		this.setNormalLevel(normalLevel);
 		
 		
+	}
+
+	public int getStockLevel() {
+		return stockLevel;
+	}
+
+	private void setStockLevel(int stockLevel) {
+		this.stockLevel = stockLevel;
+	}
+
+	public int getNormalLevel() {
+		return normalLevel;
+	}
+
+	public void setNormalLevel(int normalLevel) {
+		this.normalLevel = normalLevel;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getSellPrice() {
+		return sellPrice;
+	}
+
+	public void setSellPrice(double sellPrice) {
+		this.sellPrice = sellPrice;
 	}
 	
 	
