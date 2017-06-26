b6c301eabe53bb46cb3eff665be8e0c140002953
Gerome Marc Braddock <BraddockG@users.noreply.github.com>
2017-06-26 11:00:02 +0100

    Checkout model now adds products to an order

--- src/honesty/checkout/CheckoutModel.java
+++ src/honesty/checkout/CheckoutModel.java
@@ -7,24 +7,22 @@ import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
+import honesty.order.Order;
 import honesty.product.Product;
 import honesty.product.ProductController;
 
 public class CheckoutModel {
 
 	private String accommodation;
-	private Scanner sc;
-	private String barcode;
 	private BigDecimal basketTotal = new BigDecimal("0");
 	private ArrayList<Product> basket = new ArrayList<>();
-	private Boolean finished = false;
 	private Product currentProduct;
-	
+	private Order order;
+
 	private NumberFormat euCostFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
 
 	public CheckoutModel(String accommodation) {
 		setAccommodation(accommodation);
-		sc = new Scanner(System.in);
 		euCostFormat.setMinimumFractionDigits(2);
 		euCostFormat.setMaximumFractionDigits(2);
 		currentProduct = null;
@@ -32,34 +30,31 @@ public class CheckoutModel {
 
 	public void checkout(String barcode) {
 
-	
-			try {
-				currentProduct = ProductController.getProduct(barcode);
+		try {
+			currentProduct = ProductController.getProduct(barcode);
 
-				basketTotal = basketTotal.add(currentProduct.getSellPrice());
+			basketTotal = basketTotal.add(currentProduct.getSellPrice());
 
-				if (!basket.contains(currentProduct))
-					basket.add(currentProduct);
+			if (!basket.contains(currentProduct))
+				basket.add(currentProduct);
 
-				currentProduct.setStockLevel(currentProduct.getStockLevel() - 1);
+			currentProduct.setStockLevel(currentProduct.getStockLevel() - 1);
 
-				System.out.println("Added " + currentProduct.getProductName() + " to basket");
-				System.out.println("Current total is: " + euCostFormat.format(basketTotal));
+			System.out.println("Added " + currentProduct.getProductName() + " to basket");
+			System.out.println("Current total is: " + euCostFormat.format(basketTotal));
 
-			} catch (SQLException e) {
+		} catch (SQLException e) {
 
-				e.printStackTrace();
+			e.printStackTrace();
 
-			} catch (NullPointerException e) {
-				e.printStackTrace();
-			}
-			System.out.println("Basket size is: " + basket.size());
+		} catch (NullPointerException e) {
+			e.printStackTrace();
 		}
-
-	
+		System.out.println("Basket size is: " + basket.size());
+	}
 
 	public void finishCheckout() {
-		finished = true;
+		order.setItemList(basket);
 	}
 
 	String getAccommodation() {
